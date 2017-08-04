f8ae6d0306c499f5efc8980ff4cb4788642389c0
shwetasony <30719369+shwetasony@users.noreply.github.com>
2017-08-04 13:56:46 +0530

    Add files via upload

---
+++ pgm8.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int a;
+    float b;
+    double c;
+    char D;
+    printf("Size of int: %ld bytes\n",sizeof(a));
+    printf("Size of float: %ld bytes\n",sizeof(b));
+    printf("Size of double: %ld bytes\n",sizeof(c));
+    printf("Size of char: %ld byte\n",sizeof(D));
+    return 0;
+}
